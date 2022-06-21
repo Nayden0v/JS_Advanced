9dfc7b3c46378e907789153cb114ce335ec5c4f4
Nayden0v <vesskko23@gmail.com>
2022-06-21 17:56:19 +0300

    some exercise

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-console.log("hey");
 
-function sum(x,y){
-    return x+y;
+//creat new arr, with unique values
+
+let arr = [1,1,1,1,2,2,4,4,5,6,7,7];
+let newArr = [];
+
+for (let i = 0; i < arr.length; i++) {
+    const number = arr[i];
+    if(number > 1){
+        console.log(number);
+    }
+    
 }
 
-console.log(sum(5,6));
